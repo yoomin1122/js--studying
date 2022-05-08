38e769728bfe4565dbb4e103eb17e5b987600872
Yoo Ho <79196965+yoomin1122@users.noreply.github.com>
2022-05-08 21:38:04 +0900

    Create index.js

---
+++ day-1/index.js
@@ -0,0 +1,12 @@
+var sum; //변수 선언
+
+
+for(sum=0;6>sum;sum++) { //변수값 초기화, 6보다 작은 상태라면, 모든 일을 끝난 후 다시 반복하기 전에 증가 시킴
+    console.log("안녕하세요 현재 값은" + sum + "입니다") // 콘솔에 출력 
+}
+while(false) {
+    console.log("안녕하세요 반복문중 한개 더 있는 while이라고 합니다") // 콘솔에 출력 
+}
+while(true) {
+    console.log("안녕하세요 반복문중 한개 더 있는 while이라고 합니다") // 콘솔에 출력 
+}
